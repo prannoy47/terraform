c9c2132f8383c6d1941f8f6d60987976f203d30e
Prannoy <prannoyseal@gmail.com>
2019-03-10 15:11:01 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "Linux_VM" {
+  ami = "ami-02da3a138888ced85"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "Terraform Example"
+  }
+}
+
